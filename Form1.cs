fc2b3973c93ac61242db7798ae7c84be29d1400a
mahdi9ghasemi <ns.eslamiat@gmail.com>
2023-03-16 11:14:21 +0330

    The Source Code From vscode https://github.com/mahdi9ghasemi/vscode

---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace CSharp_EXE;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+public class HelloWorld
+
+{
+
+    public static void Main()
+
+    {
+        System.Windows.Forms.MessageBox.Show("Look Behind You");
+        System.Windows.Forms.MessageBox.Show("Are You Scard? You Can't ToNight Sleep");
+        System.Windows.Forms.MessageBox.Show("Wait To See I Think One Person She You Hacked");
+        System.Windows.Forms.MessageBox.Show("Your Computer Is Hacked!");
+        System.Windows.Forms.MessageBox.Show("Shutting Down");
+        System.Windows.Forms.MessageBox.Show("Rebooting Computer");
+        System.Windows.Forms.MessageBox.Show("Enter Your Password:");
+        System.Windows.Forms.MessageBox.Show("Enter Your Password:1");
+        System.Windows.Forms.MessageBox.Show("Enter Your Password:12");
+        System.Windows.Forms.MessageBox.Show("Enter Your Password:123");
+        System.Windows.Forms.MessageBox.Show("Enter Your Password:1234");
+        System.Windows.Forms.MessageBox.Show("Welecome");
+        System.Windows.Forms.MessageBox.Show("Downloading AntiVirus");
+        System.Windows.Forms.MessageBox.Show("Installing AntiVirus");
+        System.Windows.Forms.MessageBox.Show("Cheking For Virus");
+        System.Windows.Forms.MessageBox.Show("No Virus Computer Is Safe");
+        System.Windows.Forms.MessageBox.Show("Well, your computer does not have a virus and you can use it");
+        System.Windows.Forms.MessageBox.Show("You: Thank You");
+        System.Windows.Forms.MessageBox.Show("your welcome");
+        System.Windows.Forms.MessageBox.Show("Programing By Mahdi Ghasemi");
+        System.Windows.Forms.MessageBox.Show("I hope you liked it");
+    }
+
+}
